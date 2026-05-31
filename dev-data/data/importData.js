45e307b69e4ad24e54f781730c86e4d3686499fa
shyamcb <shyamboopathi2006@gmail.com>
2026-05-31 22:47:49 +0530

    Imported tour data

---
+++ dev-data/data/importData.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose.connect(DB).then((con) => {
+  console.log('mongodb connected');
+});
+
+const tours = JSON.parse(fs.readFileSync('dev-data/data/tours-simple.json'));
+const importTours = async () => {
+  try {
+    await Tour.create(tours);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteTours = async () => {
+  try {
+    await Tour.deleteMany();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] == '--import') importTours();
+if (process.argv[2] == '--delete') deleteTours();
