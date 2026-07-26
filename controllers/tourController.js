c1d155ec11fed292ce6e24f8eed3b5f971db539b
shyamcb <shyamboopathi2006@gmail.com>
2026-07-26 20:41:01 +0530

    Mongodb aggregation pipeline: Matching, Grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const Tour = require('../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage';
+
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     const queryObj = { ...req.query };
@@ -131,6 +138,37 @@ exports.deleteTour = async (req, res) => {
       message: null,
     });
   } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      { $match: { ratingsAverage: { $gte: 4.7 } } },
+      {
+        $group: {
+          _id: '$difficulty',
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (error) {
     res.status(400).json({
       status: 'fail',
       message: err,
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const tourController = require('../controllers/tourController');
 
 router
+  .route('/top-5-tours')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/stats').get(tourController.getTourStats);
+
+router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
+
 router
   .route('/:id')
   .get(tourController.getTour)
