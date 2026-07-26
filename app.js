5eb54dc7f7cded759f0a8b9fbc8908357673cc38
shyamcb <shyamboopathi2006@gmail.com>
2026-07-26 12:00:15 +0530

    Make the API better: Sorting, Filtering

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const tourRouter = require('./routes/tourRouter');
 
+app.set('query parser', 'extended');
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/api/v1/tours', tourRouter);
