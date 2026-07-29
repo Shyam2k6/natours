3e2c17192e0e30cc1b0d82c38ab94c0b32c3003d
shyamcb <shyamboopathi2006@gmail.com>
2026-07-29 14:00:57 +0530

    Add global error middleware

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ const express = require('express');
 
 const app = express();
 const tourRouter = require('./routes/tourRouter');
+const AppError = require('./utils/appError');
 
 app.set('query parser', 'extended');
 app.use(express.json());
 app.use(express.static('public'));
+
 app.use('/api/v1/tours', tourRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Cannot find ${req.originalUrl} in this server!`));
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  err.message = err.message || 'Server error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
